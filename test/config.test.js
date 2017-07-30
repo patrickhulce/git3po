a23d845853507eac3a1cb4076944e56c8ffcc83b
Patrick Hulce <patrick.hulce@gmail.com>
2017-07-30 08:42:01 -0700

    test: add json fixture

--- test/config.test.js
+++ test/config.test.js
@@ -11,5 +11,11 @@ describe('config.js', () => {
       expect(config.repo).to.equal('patrickhulce/git3po')
       expect(config.startAt).to.eql(new Date('2017-01-14'))
     })
+
+    it('should load a json config', () => {
+      const config = Config.from(fixture('config.json'))
+      expect(config.repo).to.equal('patrickhulce/git3po')
+      expect(config.startAt).to.eql(new Date('2017-01-14'))
+    })
   })
 })
